9a52553f6efcb43259b6e049b862b9317449a73e
Muhammed Shittu <devshittu@gmail.com>
2024-12-27 08:26:32 +0000

    remove old code

--- src/components/Dialog/HandlebarZone.tsx
+++ src/components/Dialog/HandlebarZone.tsx
@@ -1,62 +1,3 @@
-// "use client";
-
-// import React from "react";
-// import { motion } from "framer-motion";
-// import { DialogHandlebarPosition } from "./types";
-
-// type HandlebarZoneProps = {
-//   position: DialogHandlebarPosition;
-//   onPointerDown: (e: React.PointerEvent) => void;
-//   onClick: () => void;
-//   isBeyondLimit: boolean;
-// };
-
-// export function HandlebarZone({
-//   position,
-//   onPointerDown,
-//   onClick,
-//   isBeyondLimit
-// }: HandlebarZoneProps) {
-//   let zoneClasses = "";
-//   let lineClasses = "rounded-full bg-gray-300 dark:bg-gray-500";
-
-//   switch (position) {
-//     case "top":
-//       zoneClasses = "w-full h-10 flex items-center justify-center absolute top-0 cursor-pointer";
-//       lineClasses += " h-2 w-16 sm:w-12";
-//       break;
-//     case "bottom":
-//       zoneClasses = "w-full h-10 flex items-center justify-center absolute bottom-0 cursor-pointer";
-//       lineClasses += " h-2 w-16 sm:w-12";
-//       break;
-//     case "left":
-//       zoneClasses = "h-full w-10 flex items-center justify-center absolute left-0 cursor-pointer";
-//       lineClasses += " w-2 h-16 sm:h-12";
-//       break;
-//     case "right":
-//       zoneClasses = "h-full w-10 flex items-center justify-center absolute right-0 cursor-pointer";
-//       lineClasses += " w-2 h-16 sm:h-12";
-//       break;
-//   }
-
-//   return (
-//     <div
-//       className={zoneClasses}
-//       onPointerDown={onPointerDown}
-//       onClick={onClick}
-//       role="button"
-//       tabIndex={0}
-//     >
-//       <motion.div
-//         className={lineClasses}
-//         animate={{ scale: isBeyondLimit ? 1.1 : 1, backgroundColor: isBeyondLimit ? "#4B5563" : undefined }}
-//         transition={{ type: "spring", stiffness: 300, damping: 20 }}
-//       />
-//     </div>
-//   );
-// }
-
-// src/components/Dialog/HandlebarZone.tsx
 "use client";
 
 import React from "react";
@@ -103,6 +44,9 @@ export function HandlebarZone({
       className={zoneClasses}
       onPointerDown={onPointerDown}
       onClick={onClick}
+      onKeyDown={(e) => {
+        if (e.key === 'Enter' || e.key === ' ') onClick();
+      }}
       role="button"
       tabIndex={0}
       aria-label="Drag handle to close"
@@ -118,3 +62,4 @@ export function HandlebarZone({
     </div>
   );
 }
+// src/components/Dialog/HandlebarZone.tsx
