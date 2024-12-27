084bae8284188ff3d8161a77125a2ea022621323
Muhammed Shittu <devshittu@gmail.com>
2024-12-27 18:03:03 +0000

    Add card Image

---
+++ src/components/Image/CardImage.tsx
@@ -0,0 +1,55 @@
+// src/components/Image/CardImage.tsx
+'use client';
+
+import React from 'react';
+import BaseImage, { BaseImageProps } from './BaseImage';
+import clsx from 'clsx';
+
+type CardImageProps = BaseImageProps & {
+  overlayText?: React.ReactNode;
+  overlayPosition?: 'top' | 'center' | 'bottom';
+  overlayBgColor?: string; // e.g., 'bg-black bg-opacity-50'
+  caption?: string; // e.g., 'bg-black bg-opacity-50'
+};
+
+const CardImage = ({
+  overlayText,
+  overlayPosition = 'bottom',
+  overlayBgColor = 'bg-black bg-opacity-50',
+  containerClassName = '',
+  className = '',
+  caption = '',
+  ...props
+}: CardImageProps) => {
+  // Position classes
+  const positionClasses = {
+    top: 'top-0',
+    center: 'top-1/2 transform -translate-y-1/2',
+    bottom: 'bottom-0',
+  };
+
+  return (
+    <figure className={clsx('relative', containerClassName)}>
+      <BaseImage
+        shape="rounded"
+        className={clsx('rounded-lg', className)}
+        {...props}
+      />
+      {overlayText && (
+        <figcaption
+          className={clsx(
+            'absolute left-0 w-full p-2 text-white',
+            overlayBgColor,
+            positionClasses[overlayPosition],
+            'text-center'
+          )}
+          title={caption.toString()}
+        >
+          {overlayText}
+        </figcaption>
+      )}
+    </figure>
+  );
+};
+
+export default React.memo(CardImage);
