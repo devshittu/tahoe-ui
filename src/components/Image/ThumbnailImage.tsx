171ca6eec6d11d070d5bb4df10f92e1218b41f87
Muhammed Shittu <devshittu@gmail.com>
2024-12-27 18:03:43 +0000

    Add thumbnail Image

---
+++ src/components/Image/ThumbnailImage.tsx
@@ -0,0 +1,30 @@
+// src/components/Image/ThumbnailImage.tsx
+'use client';
+
+import React from 'react';
+import BaseImage, { BaseImageProps } from './BaseImage';
+import clsx from 'clsx';
+import Link from '@/components/Typography/Link'; // Assuming you have a Link component
+
+type ThumbnailImageProps = BaseImageProps & {
+  href: string;
+};
+
+const ThumbnailImage = ({
+  href,
+  containerClassName = '',
+  className = '',
+  ...props
+}: ThumbnailImageProps) => {
+  return (
+    <Link href={href} className={clsx(containerClassName)}>
+      <BaseImage
+        shape="rounded"
+        className={clsx('rounded-md', className)}
+        {...props}
+      />
+    </Link>
+  );
+};
+
+export default React.memo(ThumbnailImage);
