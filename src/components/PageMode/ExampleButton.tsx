548f402ec90ed01026dbc057aaf355a7e3343e0e
Muhammed Shittu <devshittu@gmail.com>
2024-12-29 23:21:39 +0000

    uniform with the ui/ux to encorage dynamism on theming

--- src/components/PageMode/ExampleButton.tsx
+++ src/components/PageMode/ExampleButton.tsx
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 import { useUIComponent } from '@/stores/useUIComponent';
 import { DialogExampleTrigger } from '../Dialog/DialogExampleTrigger';
 import { WelcomeScreen, WhatsNewScreen } from '@/app/playground/WelcomeScreen';
+import { BaseBox, StackBox } from '../Box';
 
 export function ExampleButton() {
   const [currentScreen, setCurrentScreen] = useState<
@@ -18,29 +19,33 @@ export function ExampleButton() {
   const { open } = useUIComponent();
   return (
     <>
-      <button
-        onClick={() =>
-          open(
-            <div>
-              Example Page Mode Content!
-              <DialogExampleTrigger />
-              <>
-                {currentScreen === 'welcome' && (
-                  <WelcomeScreen onContinue={handleContinue} />
-                )}
-                {currentScreen === 'whatsNew' && (
-                  <WhatsNewScreen onContinue={handleContinue} />
-                )}
-                <DialogExampleTrigger />
-                <DialogExampleTrigger />
-              </>
-            </div>,
-          )
-        }
-        className="px-4 py-2 bg-blue-600 text-white rounded"
-      >
-        Open Page Mode
-      </button>
+      <StackBox>
+        <BaseBox width="1/2">
+          <button
+            onClick={() =>
+              open(
+                <div>
+                  Example Page Mode Content!
+                  <DialogExampleTrigger />
+                  <>
+                    {currentScreen === 'welcome' && (
+                      <WelcomeScreen onContinue={handleContinue} />
+                    )}
+                    {currentScreen === 'whatsNew' && (
+                      <WhatsNewScreen onContinue={handleContinue} />
+                    )}
+                    <DialogExampleTrigger />
+                    <DialogExampleTrigger />
+                  </>
+                </div>,
+              )
+            }
+            className="px-4 py-2 bg-blue-600 text-white rounded"
+          >
+            Open Page Mode
+          </button>
+        </BaseBox>
+      </StackBox>
     </>
   );
 }
