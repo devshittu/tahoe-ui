49aac06c01800ad1425f8b96d444268712718eaa
Muhammed Shittu <devshittu@gmail.com>
2025-01-04 23:44:27 +0000

    add album art source

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,7 @@ const nextConfig: NextConfig = {
       'flowbite.s3.amazonaws.com', // Add external image domains here
       'your-other-domain.com', // Include any other domains hosting images
       'dummyimage.com', // Include any other domains hosting images
+      'is1-ssl.mzstatic.com', // Include any other domains hosting images
     ],
   },
 };
