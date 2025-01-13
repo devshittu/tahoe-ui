43cd403956f0bbac7cb6104e50c045b9d44ab654
Muhammed Shittu <devshittu@gmail.com>
2025-01-13 08:20:27 +0000

    feature: splash

---
+++ src/components/Splash/index.ts
@@ -0,0 +1,6 @@
+// src/components/SplashScreen/index.ts
+export { default as SplashScreen } from './splash-screen';
+export { default as SplashScreenWrapper } from './SplashScreenWrapper';
+export { withSplashScreen } from './HOC/withSplashScreen';
+export { useSplashStore } from './store/splash-store';
+export type { SplashConfig, SplashFrequency, SplashAnimation } from './splash-config';
