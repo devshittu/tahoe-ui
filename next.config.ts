25c5091a8177fde067b35a26a32dcc101007853f
Muhammed Shittu <devshittu@gmail.com>
2025-01-14 18:20:16 +0000

    add rewrites

--- next.config.ts
+++ next.config.ts
@@ -24,16 +24,20 @@ const nextConfig: NextConfig = {
     ],
   },
   async rewrites() {
-    return [
-      {
-        source: '/storybook/:path*',
-        destination: '/storybook-static/:path*',
-      },
-      {
-        source: '/storybook',
-        destination: '/storybook-static/index.html',
-      },
-    ];
+    return {
+      beforeFiles: [
+        {
+          source: '/storybook/:path*',
+          destination: '/storybook-static/:path*',
+        },
+        {
+          source: '/storybook',
+          destination: '/storybook-static/index.html',
+        },
+      ],
+      afterFiles: [],
+      fallback: [],
+    };
   },
 };
 
