5f39b58451ff3f7eac340dcb410077046c639178
Muhammed Shittu <devshittu@gmail.com>
2025-01-16 09:56:04 +0000

    add customisation

--- src/components/Splash/loading-splash.tsx
+++ src/components/Splash/loading-splash.tsx
@@ -1,6 +1,8 @@
 'use client';
+import React from 'react';
 import Icon from '../illustrations/Icon';
 import { FiCommand } from 'react-icons/fi';
+import BaseImage from '../Image/BaseImage';
 
 export const LoadingSplash: React.FC = () => {
   return (
@@ -13,32 +15,57 @@ export const LoadingSplash: React.FC = () => {
   );
 };
 
-export const IconBlockScale = () => (
-  <div
-    className={`icon block w-44 h-44 p-4  bg-cyan-200 dark:bg-cyan-700 text-cyan-500 rounded-[20px] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10`}
-  >
-    <div className={`flex justify-center items-center h-full`}>
-      <Icon
-        icon={
-          <>
+interface IconBlockScaleProps {
+  logoImage?: string;
+  logoColor?: string;
+}
+
+export const IconBlockScale: React.FC<IconBlockScaleProps> = ({
+  logoImage,
+  logoColor = 'text-cyan-500',
+}) => (
+  <div className="icon block w-44 h-44 p-4 bg-cyan-200 dark:bg-cyan-700 rounded-[20px] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
+    <div className="flex justify-center items-center h-full">
+      {logoImage ? (
+        <BaseImage
+          src={logoImage}
+          alt="Splash Logo"
+          fill
+          // width={44}
+          // height={44}
+          priority
+          // Using Tailwind classes for object fitting and overriding the default color if needed
+          className={`w-full h-full object-contain ${logoColor}`}
+        />
+      ) : (
+        <Icon
+          icon={
             <FiCommand
               className="w-12 h-12 overflow-visible animate-loader-dash stroke-current"
-              style={{
-                strokeDasharray: '0 129',
-              }}
+              style={{ strokeDasharray: '0 129' }}
             />
-          </>
-        }
-      />
+          }
+        />
+      )}
     </div>
   </div>
 );
-export const IconGrow = () => (
+
+interface IconGrowProps {
+  animationDuration?: string;
+  animationEasing?: string;
+}
+
+export const IconGrow: React.FC<IconGrowProps> = ({
+  animationDuration = '2.5s',
+  animationEasing = 'cubic-bezier(.8,0,.3,1)',
+}) => (
   <div
-    className={` icon block w-44 h-44 p-4 bg-cyan-200 dark:bg-cyan-700 rounded-[20px] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2   
-        icon_grow z-0 animate-loader-grow
-        `}
+    className="icon block w-44 h-44 p-4 bg-cyan-200 dark:bg-cyan-700 rounded-[20px] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 icon_grow z-0 animate-loader-grow"
+    style={{
+      animationDuration,
+      animationTimingFunction: animationEasing,
+    }}
   />
 );
-
-// src/components/Splash/loading-splash.tsx
+// src/components/SplashScreen/loading-splash.tsx
