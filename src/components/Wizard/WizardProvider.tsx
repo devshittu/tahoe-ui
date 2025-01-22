337ff48b4824052b9be81375d443c185f81a07dc
Muhammed Shittu <devshittu@gmail.com>
2025-01-22 20:18:34 +0000

    feat(wizard): optimize WizardProvider for stable references and performance
    - Refactored WizardProvider to use `useMemo` with shallow dependencies for `steps`, `hooks`, and `config`.
    - Ensured stable references for `steps`, `hooks`, and `config` by recommending memoization outside the provider.
    - Updated `createWizardStore` initialization to prevent unnecessary recalculations.
    - Improved type safety and added default values for the `config` object.
    - Enhanced performance by avoiding deep observation of dependencies in `useMemo`.
    
    This change reduces unnecessary store recreations and improves the overall efficiency of the wizard component.

--- src/components/Wizard/WizardProvider.tsx
+++ src/components/Wizard/WizardProvider.tsx
@@ -2,34 +2,31 @@
 
 import React, { useMemo } from 'react';
 import { WizardContext } from './hooks/useWizardStep';
-import { WizardStep, WizardHooks } from './types';
+import { WizardStep, WizardHooks, WizardConfig } from './types';
 import { createWizardStore } from './store';
 import { WizardTheme, defaultTheme } from './theme';
-import exp from 'constants';
 
+interface WizardProviderProps<TSteps extends WizardStep[]> {
+  children: React.ReactNode;
+  steps: TSteps;
+  hooks?: WizardHooks;
+  theme?: Partial<WizardTheme>;
+  config?: WizardConfig; // Accept config as a single object
+}
 export const WizardProvider = <TSteps extends WizardStep[]>({
   children,
   steps,
   hooks,
   theme = defaultTheme,
-  lazyRendering = true,
-  renderAdjacent = false,
-}: {
-  children: React.ReactNode;
-  steps: TSteps;
-  hooks?: WizardHooks;
-  theme?: Partial<WizardTheme>;
-  lazyRendering?: boolean;
-  renderAdjacent?: boolean;
-}) => {
+  config = { lazyRendering: true, renderAdjacent: false }, // Default config
+}: WizardProviderProps<TSteps>) => {
   // Initialize the store and merge the provided theme with default values.
   const useWizardStore = React.useMemo(
-    () => createWizardStore(steps, hooks, lazyRendering, renderAdjacent),
-    [steps, hooks, lazyRendering, renderAdjacent],
+    () => createWizardStore(steps, hooks, config),
+    [steps, hooks, config],
   );
   const wizardState = useWizardStore();
 
-  // const mergedTheme: WizardTheme = { ...defaultTheme, ...theme };
   // Merge the custom theme with default theme, memoizing the result for performance.
   const mergedTheme: WizardTheme = useMemo(
     () => ({ ...defaultTheme, ...theme }),
