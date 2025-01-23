6b5ff00714e987eb58226660d2a76bd4c41e5dab
Muhammed Shittu <devshittu@gmail.com>
2025-01-23 00:40:21 +0000

    feat(build): add NODE_ENV=production to build:vercel script for production builds

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,6 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === 'production' || 'staging';
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
