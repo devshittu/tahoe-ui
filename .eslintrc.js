aba29465e593cd90b04de147ca57df473d56f187
Muhammed Shittu <devshittu@gmail.com>
2025-01-24 23:26:31 +0000

    chore: update TypeScript to a stable version

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,21 +2,33 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
   },
-  extends: ['plugin:react/recommended', 'plugin:jsx-a11y/recommended', 'plugin:react-hooks/recommended', 'next/core-web-vitals', 'prettier', 'plugin:storybook/recommended', 'plugin:storybook/recommended'],
+  extends: [
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react-hooks/recommended',
+    'next/core-web-vitals',
+    'prettier',
+    'plugin:storybook/recommended',
+    'plugin:storybook/recommended',
+  ],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: ['./tsconfig.json']
+    project: ['./tsconfig.json'],
   },
   plugins: ['react', '@typescript-eslint', 'jsx-a11y', 'react-hooks'],
   rules: {
-    'react/jsx-filename-extension': [1, {
-      extensions: ['.tsx']
-    }],
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.tsx'],
+      },
+    ],
     '@typescript-eslint/no-var-requires': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off'
-  }
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'import/no-unresolved': [2, { caseSensitive: true }],
+  },
 };
