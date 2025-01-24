010f972931db041686c311b10d7e974107cc6b24
Muhammed Shittu <devshittu@gmail.com>
2025-01-24 17:21:29 +0000

    chore: optimize Storybook configuration for v8.5.1 compatibility
    - Enabled hot-reloading for local development
    - Conditional `<base href="/storybook/">` for production
    - Updated CI/CD workflow for seamless Storybook deployment on Vercel

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,7 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 
-const isProduction = process.env.NODE_ENV === 'production' || 'staging';
+const isProduction = process.env.NODE_ENV === 'production';
+
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
@@ -14,29 +15,21 @@ const config: StorybookConfig = {
     options: {},
   },
   staticDirs: ['../public'],
-  // managerHead: (head) => `
-  //   <base href="/storybook/">
-  //   ${head}
-  // `,
-  // previewHead: (head) => `
-  //   <base href="/storybook/">
-  //   ${head}
-  // `,
-
   managerHead: (head) =>
     isProduction
       ? `
-      <base href="/storybook/">
-      ${head}
-    `
+          <base href="/storybook/">
+          ${head}
+        `
       : head,
   previewHead: (head) =>
     isProduction
       ? `
-      <base href="/storybook/">
-      ${head}
-    `
+          <base href="/storybook/">
+          ${head}
+        `
       : head,
 };
 export default config;
-// .storybook/main.ts
+
+// Path: .storybook/main.ts
