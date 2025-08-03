5897f749035cc2368a9f7e3a1471cb8ff29f6dc9
Muhammed Shittu <devshittu@gmail.com>
2025-08-03 18:23:15 +0100

    feat: Add PublicLayout component for the documentation layout structure new page

---
+++ src/app/(public)/page.tsx
@@ -0,0 +1,8 @@
+// File: app/home/page.tsx
+import HeroSection from '@/components/HeroSection/HeroSection';
+
+export default function HomePage() {
+  return <HeroSection />;
+}
+
+// File: app/page.tsx
