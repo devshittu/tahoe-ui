c8d1756ab819cb0cacc9b0612e967cadecc63648
Muhammed Shittu <devshittu@gmail.com>
2025-11-29 22:27:23 +0000

    add playground page for floating control panel

---
+++ src/app/playground/modal/page.tsx
@@ -0,0 +1,15 @@
+// src/app/playground/modal/page.tsx
+'use client';
+import React, { useState } from 'react';
+import ComponentDemo from './components/demo';
+
+const DemoPage = () => {
+  return (
+    <>
+      <ComponentDemo />
+    </>
+  );
+};
+
+export default DemoPage;
+// src/app/playground/modal/page.tsx
