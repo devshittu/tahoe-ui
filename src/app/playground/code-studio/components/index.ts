86112d59ba6543bdb96ac208e960641fb080cae8
Muhammed Shittu <devshittu@gmail.com>
2026-01-18 23:39:55 +0000

    feat(code-studio): add index.ts

---
+++ src/app/playground/code-studio/components/index.ts
@@ -0,0 +1,10 @@
+// src/app/playground/code-studio/components/index.ts
+
+export { CodeStudio } from './CodeStudio';
+export { PropsPanel } from './PropsPanel';
+export { ComponentPreview } from './ComponentPreview';
+export { CodeOutput } from './CodeOutput';
+
+export * from './primitives';
+export * from './hooks';
+export * from './types';
