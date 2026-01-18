32c751ce7627606146c6dc52550bcb83e29be219
Muhammed Shittu <devshittu@gmail.com>
2026-01-18 23:38:55 +0000

    feat(code-canvas): add index.ts

---
+++ src/app/playground/code-canvas/components/primitives/index.ts
@@ -0,0 +1,12 @@
+// src/app/playground/code-canvas/components/primitives/index.ts
+export { PromptInput, default as PromptInputDefault } from './PromptInput';
+export {
+  GenerationPanel,
+  default as GenerationPanelDefault,
+} from './GenerationPanel';
+export { PreviewPanel, default as PreviewPanelDefault } from './PreviewPanel';
+export {
+  HistorySidebar,
+  default as HistorySidebarDefault,
+} from './HistorySidebar';
+export { ExportPanel, default as ExportPanelDefault } from './ExportPanel';
