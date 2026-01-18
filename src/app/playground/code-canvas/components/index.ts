5d10cc1887d80ad2fc2999ab336200c4e0019bad
Muhammed Shittu <devshittu@gmail.com>
2026-01-18 23:39:03 +0000

    feat(code-canvas): add index.ts

---
+++ src/app/playground/code-canvas/components/index.ts
@@ -0,0 +1,5 @@
+// src/app/playground/code-canvas/components/index.ts
+export { CodeCanvas, default } from './CodeCanvas';
+export * from './types';
+export * from './hooks';
+export * from './primitives';
