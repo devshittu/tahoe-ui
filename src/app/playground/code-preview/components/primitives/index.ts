4736b05333f9c6e8075463b7c5e7b90a699f9f07
Muhammed Shittu <devshittu@gmail.com>
2026-01-18 23:39:42 +0000

    feat(code-preview): add index.ts

---
+++ src/app/playground/code-preview/components/primitives/index.ts
@@ -0,0 +1,5 @@
+// src/app/playground/code-preview/components/primitives/index.ts
+
+export { CodeBlock } from './CodeBlock';
+export { CopyButton } from './CopyButton';
+export { CollapseToggle } from './CollapseToggle';
