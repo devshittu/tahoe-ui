2289fb1d6a75a49a1495f6a863250877ec2b3ce1
Muhammed Shittu <devshittu@gmail.com>
2026-01-19 23:56:20 +0000

    feat(components/wizard): add styled module barrel exports
    Export WizardCard, WizardStepper, and WizardNavigation styled components.

---
+++ src/components/Wizard/styled/index.ts
@@ -0,0 +1,14 @@
+// src/components/Wizard/styled/index.ts
+
+export { WizardStepper } from './WizardStepper';
+export {
+  WizardNavigation,
+  WizardNavigationMinimal,
+  WizardNavigationSplit,
+} from './WizardNavigation';
+export {
+  WizardCard,
+  WizardCardContainer,
+  WizardStepHeader,
+  WizardFormStep,
+} from './WizardCard';
